65118076c35b8a46c1f9fe3b21042eb064ee0a1d
Herbert Abdillah <abdillah.herbert@gmail.com>
2020-05-19 01:40:38 +0700

    add unit test

--- truffle-config.js
+++ truffle-config.js
@@ -3,9 +3,10 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      // host: "127.0.0.1",
+      host: "172.25.240.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "5777" // Match any network id
     },
     develop: {
       port: 8545
